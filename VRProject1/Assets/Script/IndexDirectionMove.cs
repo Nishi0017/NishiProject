323b51454b0433aebc3491e4301adcc019a140f5
Nishi0017 <c2216@st.cs.kumamoto-u.ac.jp>
2022-11-10 15:50:44 +0900

    bulletControllerの追加

---
+++ VRProject1/Assets/Script/IndexDirectionMove.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndexDirectionMove : MonoBehaviour
+{
+    public bool onROffL = true;
+    private char RorL = 'R';
+    public GameObject controllerObj;
+    public PlayerController controller;
+
+    public float bulletSpeed = 10;
+
+    private Rigidbody rb;
+    void Start()
+    {
+        if (onROffL)
+            RorL = 'R';
+        else
+            RorL = 'L';
+        controllerObj = GameObject.Find("OVRCustomHandPrefab_" + RorL);
+        controller = controllerObj.GetComponent<PlayerController>();
+    
+        rb = controller.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 IndexDirection = controller.indexDirection;
+        rb.AddForce(IndexDirection * bulletSpeed, ForceMode.Impulse);
+
+    }
+}
--- VRProject1/Assets/Script/PlayerController.cs
+++ VRProject1/Assets/Script/PlayerController.cs
@@ -3,21 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static OVRPlugin;
 
 public class PlayerController : MonoBehaviour
 {
     public float _threshold = 0.27f;
-    public GameObject _fire_base;
+    [Header("指パッチン")]public GameObject prefab_FingerSnap;
 
     [SerializeField]
     private OVRSkeleton _oVRSkeleton; //右手、もしくは左手の Bone情報
     [SerializeField]
     private OVRHand _oVRHand;
+
+    private bool _isThumbStraight_old;
+    private bool _isInsexStraight_old;
     private bool _isMiddleStraight_old;
+    private bool _isRingStraight_old;
+    private bool _isPinkyStraight_old;
+
+    public Vector3 indexDirection;
 
     private void Start()
     {
-        _isMiddleStraight_old = IsStraight(_threshold, OVRSkeleton.BoneId.Hand_Middle1, OVRSkeleton.BoneId.Hand_Middle2, OVRSkeleton.BoneId.Hand_Middle3, OVRSkeleton.BoneId.Hand_MiddleTip);
+        // ハンドトラッキングをしていない、またはハンドトラッキングの信用度が低ければ誤作動を防ぐために無効にする
+        if (!_oVRHand.IsTracked || _oVRHand.HandConfidence.Equals(OVRHand.TrackingConfidence.Low)) return;
+
+        var isThumbStraight_old = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Thumb0, OVRSkeleton.BoneId.Hand_Thumb1, OVRSkeleton.BoneId.Hand_Thumb2, OVRSkeleton.BoneId.Hand_Thumb3, OVRSkeleton.BoneId.Hand_ThumbTip);
+        var isIndexStraight_old = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Index1, OVRSkeleton.BoneId.Hand_Index2, OVRSkeleton.BoneId.Hand_Index3, OVRSkeleton.BoneId.Hand_IndexTip);
+        var isMiddleStraight_old = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Middle1, OVRSkeleton.BoneId.Hand_Middle2, OVRSkeleton.BoneId.Hand_Middle3, OVRSkeleton.BoneId.Hand_MiddleTip);
+        var isRingStraight_old = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Ring1, OVRSkeleton.BoneId.Hand_Ring2, OVRSkeleton.BoneId.Hand_Ring3, OVRSkeleton.BoneId.Hand_RingTip);
+        var isPinkyStraight_old = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Pinky0, OVRSkeleton.BoneId.Hand_Pinky1, OVRSkeleton.BoneId.Hand_Pinky2, OVRSkeleton.BoneId.Hand_Pinky3, OVRSkeleton.BoneId.Hand_PinkyTip);
+
     }
 
     /// <summary>
@@ -49,23 +65,47 @@ public class PlayerController : MonoBehaviour
         // ハンドトラッキングをしていない、またはハンドトラッキングの信用度が低ければ誤作動を防ぐために無効にする
         if (!_oVRHand.IsTracked || _oVRHand.HandConfidence.Equals(OVRHand.TrackingConfidence.Low)) return;
 
-        // 中指と人差し指の曲げ状況を取得する
-        // 曲げ閾値(_threshold)は0.27が丁度良かった
-        var isMiddleStraight = IsStraight(_threshold, OVRSkeleton.BoneId.Hand_Middle1, OVRSkeleton.BoneId.Hand_Middle2, OVRSkeleton.BoneId.Hand_Middle3, OVRSkeleton.BoneId.Hand_MiddleTip);
-        var isIndexStraight = IsStraight(_threshold, OVRSkeleton.BoneId.Hand_Index1, OVRSkeleton.BoneId.Hand_Index2, OVRSkeleton.BoneId.Hand_Index3, OVRSkeleton.BoneId.Hand_IndexTip);
-        // 前回のフレームで中指が伸びていて今回のフレームで中指が曲がっていれば中指を曲げた瞬間だよね
-        // そのときに人差し指が伸びていれば指パッチンをしたよね
-        if (!isMiddleStraight && _isMiddleStraight_old && isIndexStraight)
+        var isThumbStraight = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Thumb0, OVRSkeleton.BoneId.Hand_Thumb1, OVRSkeleton.BoneId.Hand_Thumb2, OVRSkeleton.BoneId.Hand_Thumb3, OVRSkeleton.BoneId.Hand_ThumbTip);
+        var isIndexStraight = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Index1, OVRSkeleton.BoneId.Hand_Index2, OVRSkeleton.BoneId.Hand_Index3, OVRSkeleton.BoneId.Hand_IndexTip);
+        var isMiddleStraight = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Middle1, OVRSkeleton.BoneId.Hand_Middle2, OVRSkeleton.BoneId.Hand_Middle3, OVRSkeleton.BoneId.Hand_MiddleTip);
+        var isRingStraight = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Ring1, OVRSkeleton.BoneId.Hand_Ring2, OVRSkeleton.BoneId.Hand_Ring3, OVRSkeleton.BoneId.Hand_RingTip);
+        var isPinkyStraight = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Pinky0, OVRSkeleton.BoneId.Hand_Pinky1, OVRSkeleton.BoneId.Hand_Pinky2, OVRSkeleton.BoneId.Hand_Pinky3, OVRSkeleton.BoneId.Hand_PinkyTip);
+
+        var indexTipPos = _oVRSkeleton.Bones[(int)OVRSkeleton.BoneId.Hand_IndexTip].Transform.position;
+        var index1Pos = _oVRSkeleton.Bones[(int)OVRSkeleton.BoneId.Hand_Index1].Transform.position;
+        
+        float indexDirection_x = indexTipPos.x - index1Pos.x;
+        float indexDirection_y = indexTipPos.y - index1Pos.y;
+        float indexDirection_z = indexTipPos.z - index1Pos.z;
+
+        indexDirection = new Vector3(indexDirection_x, indexDirection_y, indexDirection_z);
+ 
+
+        //指パッチン
+        if (_isMiddleStraight_old && !isMiddleStraight && isIndexStraight)
         {
-            // プレハブ化しておいた炎のエフェクトを生成する
-            // 生成場所は人差し指の先にするとそれっぽい
             Instantiate(
-                _fire_base,
-                position: _oVRSkeleton.Bones[(int)OVRSkeleton.BoneId.Hand_IndexTip].Transform.position,
-                rotation: Quaternion.identity
+                prefab_FingerSnap, //クローンするプレハブ
+                position: _oVRSkeleton.Bones[(int)OVRSkeleton.BoneId.Hand_IndexTip].Transform.position, //生成場所：人差し指の先
+                rotation: Quaternion.identity //回転なし
             );
         }
 
+
+        if(_isThumbStraight_old && !isThumbStraight && isIndexStraight && !isMiddleStraight && !isRingStraight && !isPinkyStraight)
+        {
+            Instantiate(
+                prefab_FingerSnap, //クローンするプレハブ
+                position: _oVRSkeleton.Bones[(int)OVRSkeleton.BoneId.Hand_IndexTip].Transform.position, //生成場所：人差し指の先
+                rotation: Quaternion.identity //回転なし
+            );
+        }
+
+
+        _isThumbStraight_old = isThumbStraight;
+        _isInsexStraight_old = isIndexStraight;
         _isMiddleStraight_old = isMiddleStraight;
-    }
+        _isRingStraight_old = isRingStraight;
+        _isPinkyStraight_old = isPinkyStraight;
+}
 }
